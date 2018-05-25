23adf9de3c9a41aeec90ae7049cb9781d4844d26
Rats <juliocesarmmc@hotmail.com>
2018-05-25 19:50:29 -0300

    Comentado Trim

--- trim.c
+++ trim.c
@@ -1,10 +1,8 @@
-#include <stdlib.h>
-#include <string.h>
 #include "trim.h"
-#include <stdio.h>
 
 char *Trim(char *line){
     int init=0, last=0;
+    // pega o primeiro e ultimo indice com algum simbolo
     for(int i=0;i<strlen(line);i++){
         if(line[i]==' '){
             if(last==0){
@@ -15,6 +13,7 @@ char *Trim(char *line){
             last=i;
         }
     }
+    // cria a nova string sem espaços no começo e no fim
     char *trim_txt = (char *)malloc(sizeof(char)*(last-init+2));
     for(int i=0;i<((last-init+1));i++){
        trim_txt[i]=line[init+i];
--- trim.h
+++ trim.h
@@ -1,5 +1,9 @@
 #ifndef TRIM
 #define  TRIM
+#include <stdlib.h>
+#include <string.h>
+
+// retira espaços no inicio e no fim de uma string
 
 char *Trim(char *line);
 
